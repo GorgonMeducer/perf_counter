f9a27d4ba0ea99bf4268bf21bfcc638944ac09cd
Gabriel Wang <embedded_zhuoran@Hotmail.com>
2021-03-24 11:04:52 +0000

    update example coding style

--- example/main.c
+++ example/main.c
@@ -97,8 +97,7 @@ int main (void)
     //! demo of using clock() in timer.h
     do {
         clock_t tStart = clock();
-        safe_atom_code() 
-        {
+        safe_atom_code() {
             printf("no interrupt \r\n");
         }
         printf("used clock cycle: %d", (int32_t)(clock() - tStart));
