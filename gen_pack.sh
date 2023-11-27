39741caac72c082d1e388dc6a940e4b46360563c
Gabriel Wang <gabriel.wang@arm.com>
2023-11-27 22:57:58 +0000

    prpare using CI for generate cmsis-pack

--- gen_pack.sh
+++ gen_pack.sh
@@ -1,223 +1,151 @@
-#!/bin/bash
-# Version: 1.1 
-# Date: 2020-04-29
+#!/usr/bin/env bash
+# Version: 2.7
+# Date: 2023-05-22
 # This bash script generates a CMSIS Software Pack:
 #
-# Pre-requisites:
-# - bash shell (for Windows: install git for Windows)
-# - 7z in path (zip archiving utility)
-#   e.g. Ubuntu: sudo apt-get install p7zip-full p7zip-rar) 
-# - PackChk in path with execute permission
-#   (see CMSIS-Pack: CMSIS/Utilities/<os>/PackChk)
-# - xmllint in path (XML schema validation)
-#   e.g. Ubuntu: sudo apt-get install libxml2-utils
-#   Windows: download from https://www.zlatkovic.com/pub/libxml/
 
-############### EDIT BELOW ###############
-# Extend Path environment variable locally
+set -o pipefail
+
+# Set version of gen pack library
+# For available versions see https://github.com/Open-CMSIS-Pack/gen-pack/tags.
+# Use the tag name without the prefix "v", e.g., 0.7.0
+REQUIRED_GEN_PACK_LIB="0.8.4"
+
+# Set default command line arguments
+DEFAULT_ARGS=(-c "v")
+
+# Pack warehouse directory - destination
+# Default: ./output
 #
-if [ `uname -s` = "Linux" ]
-  then
-  CMSIS_PACK_PATH="/home/$USER/.arm/Packs/ARM/CMSIS/5.9.0/"
-  PATH_TO_ADD="$CMSIS_PACK_PATH/CMSIS/Utilities/Linux64/"
-else
-  CMSIS_PACK_PATH="/C/Users/gabriel/AppData/Local/Arm/Packs/ARM/CMSIS/5.9.0"
-  PATH_TO_ADD="/C/Program Files (x86)/7-Zip/:$CMSIS_PACK_PATH/CMSIS/Utilities/Win32/:/C/xmllint/"
-fi
-[[ ":$PATH:" != *":$PATH_TO_ADD}:"* ]] && PATH="${PATH}:${PATH_TO_ADD}"
-echo $PATH_TO_ADD appended to PATH
-echo " "
+# PACK_OUTPUT=./output
 
-# Pack warehouse directory - destination 
-PACK_WAREHOUSE=cmsis-pack/
+# Temporary pack build directory,
+# Default: ./build
+#
+# PACK_BUILD=./build
 
-# Temporary pack build directory
-PACK_BUILD=build/
+# Specify directory names to be added to pack base directory
+# An empty list defaults to all folders next to this script.
+# Default: empty (all folders)
+#
+ PACK_DIRS="
+   benchmark
+   documents
+   os
+   lib
+ "
 
-# Specify directories included in pack relative to base directory
-# All directories:
-# PACK_DIRS=`ls -d */`
-# Do not include the build directory if it is local
-# PACK_DIRS=${PACK_DIRS//$PACK_BUILD/}
-# PACK_DIRS=${PACK_DIRS//$PACK_WAREHOUSE/}
-
-# alternative: specify directory names to be added to pack base directory
-PACK_DIRS="
-  lib
-  os
-  documents
-  benchmark
-"
-
-  
 # Specify file names to be added to pack base directory
-PACK_BASE_FILES="
-  perf_counter.c
-  perf_counter.h
-  systick_wrapper_gcc.s
-  systick_wrapper_gnu.s
-  systick_wrapper_ual.s
-  LICENSE
-  README.md
-"
+# Default: empty
+#
+ PACK_BASE_FILES="
+   LICENSE
+   perf_counter.c
+   perf_counter.h
+   README.md
+   systick_wrapper_gcc.s
+   systick_wrapper_gnu.s
+   systick_wrapper_ual.s
+ "
+
+# Specify file names to be deleted from pack build directory
+# Default: empty
+#
+# PACK_DELETE_FILES="
+# "
+
+# Specify patches to be applied
+# Default: empty
+#
+# PACK_PATCH_FILES="
+#     <list patches here>
+# "
+
+# Specify addition argument to packchk
+# Default: empty
+#
+# PACKCHK_ARGS=()
+
+# Specify additional dependencies for packchk
+# Default: empty
+#
+# PACKCHK_DEPS="
+#   <list pdsc files here>
+# "
+
+# Optional: restrict fallback modes for changelog generation
+# Default: full
+# Values:
+# - full      Tag annotations, release descriptions, or commit messages (in order)
+# - release   Tag annotations, or release descriptions (in order)
+# - tag       Tag annotations only
+#
+# PACK_CHANGELOG_MODE="<full|release|tag>"
+
+#
+# custom pre-processing steps
+#
+# usage: preprocess <build>
+#   <build>  The build folder
+#
+function preprocess() {
+  # add custom steps here to be executed
+  # before populating the pack build folder
+  return 0
+}
+
+#
+# custom post-processing steps
+#
+# usage: postprocess <build>
+#   <build>  The build folder
+#
+function postprocess() {
+  # add custom steps here to be executed
+  # after populating the pack build folder
+  # but before archiving the pack into output folder
+  return 0
+}
 
 ############ DO NOT EDIT BELOW ###########
-echo Starting CMSIS-Pack Generation: `date`
-# Zip utility check 
-ZIP=7z
-type -a $ZIP
-errorlevel=$?
-if [ $errorlevel -gt 0 ]
-  then
-  echo "Error: No 7zip Utility found"
-  echo "Action: Add 7zip to your path"
-  echo " "
-  exit
-fi
 
-# Pack checking utility check
-PACKCHK=PackChk
-type -a $PACKCHK
-errorlevel=$?
-if [ $errorlevel != 0 ]
-  then
-  echo "Error: No PackChk Utility found"
-  echo "Action: Add PackChk to your path"
-  echo "Hint: Included in CMSIS Pack:"
-  echo "<pack_root_dir>/ARM/CMSIS/<version>/CMSIS/Utilities/<os>/"
-  echo " "
-  exit
-fi
-echo " "
+function install_lib() {
+  local URL="https://github.com/Open-CMSIS-Pack/gen-pack/archive/refs/tags/v$1.tar.gz"
+  local STATUS=$(curl -sLI "${URL}" | grep "^HTTP" | tail -n 1 | cut -d' ' -f2 || echo "$((600+$?))")
+  if [[ $STATUS -ge 400 ]]; then
+    echo "Wrong/unavailable gen-pack lib version '$1'!" >&2
+    echo "Check REQUIRED_GEN_PACK_LIB variable."  >&2
+    echo "For available versions see https://github.com/Open-CMSIS-Pack/gen-pack/tags." >&2
+    exit 1
+  fi
+  echo "Downloading gen-pack lib version '$1' to '$2' ..."
+  mkdir -p "$2"
+  curl -L "${URL}" -s | tar -xzf - --strip-components 1 -C "$2" || exit 1
+}
 
-# XML syntax checking utility check
-XMLLINT=xmllint
-type -a $XMLLINT
-errorlevel=$?
-if [ $errorlevel != 0 ]
-  then
-  echo "Error: No xmllint found"
-  echo "Action: Add xmllint to your path"
-  echo " "
-  exit
-fi
-echo " "
+function load_lib() {
+  if [[ -d ${GEN_PACK_LIB} ]]; then
+    . "${GEN_PACK_LIB}/gen-pack"
+    return 0
+  fi
+  local GLOBAL_LIB="/usr/local/share/gen-pack/${REQUIRED_GEN_PACK_LIB}"
+  local USER_LIB="${HOME}/.local/share/gen-pack/${REQUIRED_GEN_PACK_LIB}"
+  if [[ ! -d "${GLOBAL_LIB}" && ! -d "${USER_LIB}" ]]; then
+    echo "Required gen_pack lib not found!" >&2
+    install_lib "${REQUIRED_GEN_PACK_LIB}" "${USER_LIB}"
+  fi
 
-# Locate Package Description file
-# check whether there is more than one pdsc file
-NUM_PDSCS=`ls -1 *.pdsc | wc -l`
-PACK_DESCRIPTION_FILE=`ls *.pdsc`
-if [ $NUM_PDSCS -lt 1 ]
-  then
-  echo "Error: No *.pdsc file found in current directory"
-  echo " "
-elif [ $NUM_PDSCS -gt 1 ]
-  then
-  echo "Error: Only one PDSC file allowed in directory structure:"
-  echo "Found:"
-  echo "$PACK_DESCRIPTION_FILE"
-  echo "Action: Delete unused pdsc files"
-  echo " "
-  exit
-fi
+  if [[ -d "${GLOBAL_LIB}" ]]; then
+    . "${GLOBAL_LIB}/gen-pack"
+  elif [[ -d "${USER_LIB}" ]]; then
+    . "${USER_LIB}/gen-pack"
+  else
+    echo "Required gen-pack lib is not installed!" >&2
+    exit 1
+  fi
+}
 
-SAVEIFS=$IFS
-IFS=.
-set $PACK_DESCRIPTION_FILE
-# Pack Vendor
-PACK_VENDOR=$1
-# Pack Name
-PACK_NAME=$2
-echo Generating Pack Version: for $PACK_VENDOR.$PACK_NAME
-echo " "
-IFS=$SAVEIFS
+load_lib
+gen_pack "${DEFAULT_ARGS[@]}" "$@"
 
-#if $PACK_BUILD directory does not exist, create it.
-if [ ! -d $PACK_BUILD ]; then
-  mkdir -p $PACK_BUILD
-fi
-
-# Copy files into build base directory: $PACK_BUILD
-# pdsc file is mandatory in base directory:
-cp -f  ./$PACK_VENDOR.$PACK_NAME.pdsc ${PACK_BUILD}
-
-cp -f ./os/perf_os_patch_rtx5.c ./lib
-cp -f ./os/perf_os_patch_freertos.c ./lib
-cp -f ./os/perf_os_patch_rt_thread.c ./lib
-cp -f ./os/perf_os_patch_threadx.c ./lib
-cp -f ./perf_counter.h ./lib
-
-# directories
-echo Adding directories to pack:
-echo $PACK_DIRS
-echo " "
-for d in ${PACK_DIRS}
-do
-  cp -r "$d" ${PACK_BUILD}
-done
-
-# files for base directory
-echo Adding files to pack:
-echo $PACK_BASE_FILES
-echo " "
-for f in $PACK_BASE_FILES
-do 
-  cp -f  "$f" $PACK_BUILD/ 
-done
-
-
-
-# Run Schema Check (for Linux only):
-# sudo apt-get install libxml2-utils
-
-echo Running schema check for $PACK_VENDOR.$PACK_NAME.pdsc
-$XMLLINT --noout --schema ${CMSIS_PACK_PATH}/CMSIS/Utilities/PACK.xsd $PACK_BUILD/$PACK_VENDOR.$PACK_NAME.pdsc
-errorlevel=$?
-if [ $errorlevel -ne 0 ]; then
-  echo "build aborted: Schema check of $PACK_VENDOR.$PACK_NAME.pdsc against PACK.xsd failed"
-  echo " "
-  exit
-fi
-
-# Run Pack Check and generate PackName file with version
-$PACKCHK $PACK_BUILD/$PACK_VENDOR.$PACK_NAME.pdsc -n PackName.txt -x M362
-errorlevel=$?
-if [ $errorlevel -ne 0 ]; then
-  echo "build aborted: pack check failed"
-  echo " "
-  exit
-fi
-
-PACKNAME=`cat PackName.txt`
-rm -rf PackName.txt
-
-# Archiving
-# $ZIP a $PACKNAME
-echo creating pack file $PACKNAME
-#if $PACK_WAREHOUSE directory does not exist create it
-if [ ! -d $PACK_WAREHOUSE ]; then
-  mkdir -p $PACK_WAREHOUSE
-fi
-pushd $PACK_WAREHOUSE
-PACK_WAREHOUSE=`pwd`
-popd
-pushd $PACK_BUILD
-"$ZIP" a $PACK_WAREHOUSE/$PACKNAME -tzip
-popd
-errorlevel=$?
-if [ $errorlevel -ne 0 ]; then
-  echo "build aborted: archiving failed"
-  exit
-fi
-
-cp -f ./$PACK_VENDOR.$PACK_NAME.pdsc ${PACK_WAREHOUSE}
-
-
-echo "build of pack succeeded"
-# Clean up
-echo "cleaning up ..."
-
-rm -rf $PACK_BUILD
-echo " "
-
-echo Completed CMSIS-Pack Generation: `date`
+exit 0
