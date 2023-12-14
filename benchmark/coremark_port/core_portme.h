6d8303cd7146b4b0bf873413f5f403de5889ce1f
Gabriel Wang <gabriel.wang@arm.com>
2023-12-14 03:00:41 +0000

    Update core_portme.h

--- benchmark/coremark_port/core_portme.h
+++ benchmark/coremark_port/core_portme.h
@@ -103,9 +103,7 @@ typedef int64_t CORE_TICKS;
 #define COMPILER_FLAGS \
     "Unspecified" /* "Please put compiler flags here (e.g. -o3)" */
 #endif
-#ifndef MEM_LOCATION
-#define MEM_LOCATION "STATIC"
-#endif
+
 
 /* Data Types :
         To avoid compiler issues, define the data types that need ot be used for
@@ -151,8 +149,18 @@ typedef size_t         ee_size_t;
         MEM_STACK - to allocate the data block on the stack (NYI).
 */
 #ifndef MEM_METHOD
-#define MEM_METHOD MEM_STATIC
+#define MEM_METHOD MEM_STACK
 #endif
+
+#undef MEM_LOCATION
+#if     MEM_METHOD == MEM_MALLOC
+#   define MEM_LOCATION "MALLOC"
+#elif   MEM_METHOD == MEM_STATIC
+#   define MEM_LOCATION "STATIC"
+#elif   MEM_METHOD == MEM_STACK
+#   define MEM_LOCATION "STACK"
+#endif
+
 
 /* Configuration : MULTITHREAD
         Define for parallel execution
@@ -205,7 +213,7 @@ typedef size_t         ee_size_t;
 #endif
 
 #ifndef ITERATIONS
-#define ITERATIONS  2000
+#define ITERATIONS  3000
 #endif
 
 /* Variable : default_num_contexts
