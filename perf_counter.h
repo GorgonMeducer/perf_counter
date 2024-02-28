c213ffd46de7779d5575eece6337551a3f11993a
Gabriel Wang <gabriel.wang@arm.com>
2024-02-28 20:33:15 +0000

    Update perf_counter.h

--- perf_counter.h
+++ perf_counter.h
@@ -42,7 +42,7 @@ extern "C" {
 #define __PERF_COUNTER_VER_MINOR__          3
 #define __PERF_COUNTER_VER_REVISE__         0
 
-#define __PERF_COUNTER_VER_STR__            "dev"
+#define __PERF_COUNTER_VER_STR__            ""
 
 #define __PER_COUNTER_VER__    (__PERF_COUNTER_VER_MAJOR__ * 10000ul            \
                                +__PERF_COUNTER_VER_MINOR__ * 100ul              \
