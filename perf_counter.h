252c3e50e810b1b45c58158d8bf9b529a307e270
Gabriel Wang <gabriel.wang@arm.com>
2026-01-08 01:41:29 +0000

    add ICache miss calculation in PMU

--- perf_counter.h
+++ perf_counter.h
@@ -48,9 +48,9 @@ extern "C" {
  */
 #define __PERF_COUNTER_VER_MAJOR__          2
 #define __PERF_COUNTER_VER_MINOR__          5
-#define __PERF_COUNTER_VER_REVISE__         4
+#define __PERF_COUNTER_VER_REVISE__         5
 
-#define __PERF_COUNTER_VER_STR__            ""
+#define __PERF_COUNTER_VER_STR__            "dev"
 
 #define __PER_COUNTER_VER__    (__PERF_COUNTER_VER_MAJOR__ * 10000ul            \
                                +__PERF_COUNTER_VER_MINOR__ * 100ul              \
